3739ab9700270a68a116e0415288f5d50019c87b
Travis Bigelow <tbigelow22@gmail.com>
2016-04-05 21:12:05 -0600

    I am dumb and can't spelling settings. Fixed it so that it will run although nothing happens right now.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,35 +1,39 @@
-import sys
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
-	# Initialize pygame, settings, and create a screen object
+	# Initialize pygame, settings, and screen object
 	pygame.init()
 	ai_settings = Settings()
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Mutha Fuckin' Alien Invasion")
 
-	# Make a ship.
-	ship = Ship(screen)
-
 	# Set the background color.
 	bg_color = (230,230,230)
+
+	# Make a ship.
+	ship = Ship(screen)
 
 	# Start the main loop for the game.
 	while True:
 
-		# Watch for keyboard and mouse events.
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
+		# # Watch for keyboard and mouse events.
+		# for event in pygame.event.get():
+		# 	if event.type == pygame.QUIT:
+		# 		sys.exit()
+		# replace the above with the next line of code:
+		gf.check_events(ship)
+		ship.update()
 
-		# Redraw the screen during each pass through the loop.
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
+		# # Redraw the screen during each pass through the loop.
+		# screen.fill(ai_settings.bg_color)
+		# ship.blitme()
+		# Refactored to the below code:
+		gf.update_screen(ai_settings, screen, ship)
 
 		# Make the most recently drawn screen visible.
 		pygame.display.flip()
